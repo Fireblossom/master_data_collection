300e43373ed63770fa3355ce07d4d899374f4fa7
Changxu Duan <duancu@kiwi.ims.uni-stuttgart.de>
2020-11-17 16:10:43 +0100

    17.11

--- py_tec/save_pickle.py
+++
@@ -1,6 +0,0 @@
-from py_tec.tec_loader_spacy import TecLoader
-loader = TecLoader(tokenize=True, augment=True)
-dataset = loader.load_tec('./tec.txt')
-import pickle
-pickle.dump(dataset, open('tec_augment.pkl', 'wb'))
-pickle.l
